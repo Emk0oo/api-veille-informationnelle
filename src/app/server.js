c843bce7b066e560bbc497673ee3acf241d617ac
Javor Emir <emir.j2002@gmail.com>
2025-03-15 15:11:56 +0100

    init auth archi

--- src/app/server.js
+++ src/app/server.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.API_PORT;
+const db = require("./config/db");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
