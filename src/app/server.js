0b43a5c9a1abae41e62ee6205d35f63e3c717c67
Javor Emir <emir.j2002@gmail.com>
2025-03-20 23:24:09 +0100

    cors + fix cors + env ref

--- src/app/server.js
+++ src/app/server.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
+const cors = require('cors'); // Ajoutez cette ligne pour importer cors
 dotenv.config();
 const db = require("./config/db");
 const verifyToken = require("./middleware/verifyTokenValidity");
@@ -13,6 +14,13 @@ const rssFeedService = require("./services/rssFeeds.service");
 
 const port = process.env.API_PORT;
 
+// Configurer CORS - à placer AVANT les autres middlewares
+app.use(cors({
+  origin: process.env.FRONTEND_URL, // L'origine de votre frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,8 +29,6 @@ app.use("/api/auth", authRoute);
 app.use(verifyToken);
 app.use("/api/rssFeeds", rssFeedsRoute);
 app.use("/api/subscriptions", subscriptionRoute);
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
